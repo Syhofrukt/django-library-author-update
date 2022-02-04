276bb5ba35e3dc7c5eb94c8d9acdb23a58b6101e
Syhofrukt <duboviylistik@gmail.com>
2022-02-04 20:56:13 +0200

    main files

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("books", views.BookListView.as_view(), name="books"),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book"),
+    path("authors", views.AuthorListView.as_view(), name="authors"),
+    path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author"),
+]
